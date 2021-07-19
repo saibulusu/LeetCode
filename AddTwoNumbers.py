3f708b8234518e81a242f1a3986c3ab02e3a532d
Sai Bulusu <sai.bulusu@gmail.com>
2021-07-18 23:30:00 -0700

    Update AddTwoNumbers.py

--- AddTwoNumbers.py
+++ AddTwoNumbers.py
@@ -1,37 +1,35 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(   x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        if (!l2 && l1->val == 0 && !l1->next) {
-            return nullptr;
-        }
-        if (l2) {
-            l1->val += l2->val;
-        }
-
-        if (!l1->next) {
-            l1->next = new ListNode(l1->val / 10, nullptr);
-        } else {
-            l1->next->val += l1->val / 10;
-        }
-
-        l1->val %= 10;
-        
-        if (l2) {
-            l1->next = addTwoNumbers(l1->next, l2->next);
-        } else {
-            l1->next = addTwoNumbers(l1->next, nullptr);
-        }
-
-        return l1;
-    }
-};
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        return self.recurse(l1, l2, 0)
+    
+    def recurse(self, l1: ListNode, l2: ListNode, carry: int) -> ListNode:
+        if l1 is None and l2 is None:
+            if carry == 0:
+                return None
+            else:
+                return ListNode(carry, None)
+        else:
+            l1Next = None
+            l2Next = None
+            carryNext = 0
+            curSum = 0
+            if l1 is None and l2 is not None:
+                curSum = carry + l2.val
+                l1Next = None
+                l2Next = l2.next
+            elif l1 is not None and l2 is None:
+                curSum = carry + l1.val
+                l1Next = l1.next
+                l2Next = None                
+            else:
+                curSum = carry + l2.val + l1.val
+                l1Next = l1.next
+                l2Next = l2.next
+            curVal = curSum % 10
+            carryNext = curSum // 10
+            return ListNode(curVal, self.recurse(l1Next, l2Next, carryNext))
